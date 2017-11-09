a7e16a437cd75447c3c239d1d5ffa85762ffec21
[01;31m[K"name"[m[K: "Cate <[01;31m[K"email"[m[K: null>
2017-11-09 15:20:05 -0600

    generate category model/migrations and join table migration, update associations

--- db/migrate/20171109202029_create_keywords.rb
+++ db/migrate/20171109202029_create_keywords.rb
@@ -1,8 +1,9 @@
 class CreateKeywords < ActiveRecord::Migration[5.1]
   def change
     create_table :keywords do |t|
-      t.string :word
-      t.string :img_src
+      t.string :word, null: false
+      t.string :img_src, null: false
+      t.integer :user_id, default: null
 
       t.timestamps
     end
