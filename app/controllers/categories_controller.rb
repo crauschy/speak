f121d776258d9ddc0b7ea687fbb51c11eee407ba
Gaby Ramirez <GabrielaSRamirez94@gmail.com>
2017-11-10 16:58:21 -0600

    redo of tile algorithm - okay not actual styling

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,33 +1,8 @@
 class CategoriesController < ApplicationController
 	def index
-		@categories = Category.all
-		# PUT INTO MODEL AS CATEGORIES_RENDER METHOD
-		cols = 1
-		rows = 1
-		@html = ""
-		@categories.each do |category|
-			if category.name != "Keyboard"
-				if cols == 1
-					@html = "<div class='row words-row categories-row row-#{rows}'>"
-				end
-				@html += "<div class='col-md-1 col-#{cols} words-col categories-col'>"
-				@html += "#{category.name}"
-				@html += "<img src='/assets/icons/#{category.img_src}', alt='Icon', height='40px'>"
-				@html += "</div>"
-			end
-			if (cols == 12 || @categories.last.name == category.name)
-				@html += "</div>"
-				cols = 0
-				rows += 1
-			end
-			cols += 1
-		end
+		@categories = Category.where.not(name: 'Keyboard')
 		if request.xhr?
-			render json: { HTML: @html }
-		else
-			@html
+			render json: { html: render_to_string("partials/_categories_render", layout: false) }
 		end
 	end
-
-
 end
