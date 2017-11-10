647c4c913f5f94c4b67c3cc2e30740931741e97e
Vincent Lu <vincentlu@Vincents-MacBook-Pro.local>
2017-11-09 18:44:25 -0600

    completed seed mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+data = {Food: ["pizza", "banana", "carrot", "burger", "bread", "breakfast", "juice", "hotdog", "water", "milk"], People: ["me", "you", "us", "sister", "teacher", "brother", "mom", "dad", "family", "friend", "grandparents"], Emotions: ["happy", "sad", "angry", "scared", "excited", "sick", "love", "nervous", "sleepy"], Colors: ["yellow", "blue", "red", "green", "orange", "purple", "white", "black", "brown", "pink"], Location_Helpers: ["right", "left", "up", "down", "close", "far", "under", "there", "here", "in", "out", "on top of"], Action: ["see", "hear", "get", "go", "stop", "eat", "drink", "walk", "stop", "wash", "play", "open", "close", "touch", "brush", "draw"], Places: ["home", "school", "playground", "outside", "hospital", "restaurant", "park", "room", "bathroom"], Animals: ["dog", "cat", "horse", "fish", "turtle", "bird", "bunny", "pig", "chicken", "horse"], Adjectives: ["hot", "cold", "wet", "loud", "quiet", "dry", "hard", "soft", "dirty", "clean", "big", "little"], Nature: ["trees", "flower", "sun", "moon", "cloud", "leaves", "grass", "stars", "rain"], Text: ["want", "help", "need", "I am", "please", "thank you", "you are", "what", "where", "when", "yes", "no"]}
+
+data.each do |category, keyword|
+
+	category_object = Category.create(name: category)
+	i = 0
+	while i < keyword.length
+		keyword_object = Keyword.create(word: keyword[i])
+		keyword_object.categories << category_object
+		i += 1
+	end
+
+end
