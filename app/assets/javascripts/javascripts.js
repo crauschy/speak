2ddff6586c848d7c2529a3a7053e9d7a23eedae3
khristoph <christopherfriedman@gmail.com>
2017-11-10 16:25:16 -0600

    attempt to fix heroku image issue

--- app/assets/javascripts/javascripts.js
+++ app/assets/javascripts/javascripts.js
@@ -1,6 +1,9 @@
+
+//heroku config:set AWS_ACCESS_KEY_ID='AKIAJQYN4XFRSQJJI3QA' AWS_SECRET_ACCESS_KEY=yyy
+
 AWS.config.region = 'us-east-2';
-AWS.config.accessKeyId = 'AKIAJU3RZQPBVXFGFKFQ';
-AWS.config.secretAccessKey = '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';
+AWS.config.accessKeyId = 'AKIAJQYN4XFRSQJJI3QA';
+AWS.config.secretAccessKey = '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';
 
 
 
--- config/initializers/aws.rb
+++
@@ -1,4 +0,0 @@
-Aws.config.update({
-  region: 'us-east-2',
-  credentials: Aws::Credentials.new('AKIAJ3QPH4QTKKKGNQCA', '13ufr1Xbt8fVgP3tPXlv26dAVS3KiVKeBw+040o4')
-  })
