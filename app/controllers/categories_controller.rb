82a41f132c33e3ea4ea02cadcaa34b678f0717a5
khristoph <christopherfriedman@gmail.com>
2017-11-11 12:40:28 -0600

    testing image_tag

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,14 @@ class CategoriesController < ApplicationController
 			@most_used = @user.phrases.order(count: :desc)[0..2]
 			@most_recent = @user.phrases.order(updated_at: :desc)[0..2]
 		end
+		@categories.each do |category|
+			p category
+			p category.img_src
+			p ActionController::Base.helpers.image_tag('icons/'+ category.img_src, height: "40px")
+			p "==================================="
+			p ''
+			p ''
+		end
 		if request.xhr?
 			render json: { html: render_to_string("partials/_categories_render", layout: false) }
 		end
