3118f1eb4a47e7fa3a08b191e79d42d3785997b7
khristoph <christopherfriedman@gmail.com>
2017-11-12 13:23:51 -0600

    ~200 words

--- app/controllers/speak_controller.rb
+++ app/controllers/speak_controller.rb
@@ -2,14 +2,14 @@ class SpeakController < ActionController::Base
 
   def index
 
-  Aws.config.update({
-  region: 'us-east-2',
-  credentials: Aws::Credentials.new('AKIAIR2UXYLVWMXTTAPQ', 'JwGI9Hfc8z8A1Qr4Iiodfy5SQxHxwGp/W5StM/AW')
-  })
-  speech = "potato"
-  # binding.pry
-   binding.pry
-  speech = Aws::Polly::Client.new(region: 'us-east-2').synthesize_speech(output_format: 'mp3', text:'Hello world', voice_id: 'Joanna').audio_stream
+  # Aws.config.update({
+  # region: 'us-east-2',
+  # credentials: Aws::Credentials.new('AKIAIR2UXYLVWMXTTAPQ', 'JwGI9Hfc8z8A1Qr4Iiodfy5SQxHxwGp/W5StM/AW')
+  # })
+  # speech = "potato"
+  # # binding.pry
+
+  # speech = Aws::Polly::Client.new(region: 'us-east-2').synthesize_speech(output_format: 'mp3', text:'Hello world', voice_id: 'Joanna').audio_stream
 
 
   end
