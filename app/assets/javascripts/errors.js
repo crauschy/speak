6330a36d4a799baabff635bbb27edee8ac52641c
[01;31m[K"name"[m[K: "Cate <[01;31m[K"email"[m[K: null>
2017-11-14 10:06:05 -0600

    working on errors

---
+++ app/assets/javascripts/errors.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+//   var signUp = function() {
+//     $.ajax({
+//       url: "/users",
+//       type: "POST",
+
+//     })
+//     .done(function(response) {
+//       console.log(response)
+//       // $(".modal-body").append(response.html);
+//     })
+//   }
+
+
+//   $('form#new_user').on('submit', function(){
+
+//     event.preventDefault()
+//     console.log("submitted")
+//     signUp()
+//     console.log("testing")
+//   })
+// })
