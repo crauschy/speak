c33a518f01c86de59807e051fbbce95402e22c3a
khristoph <christopherfriedman@gmail.com>
2017-11-14 15:30:43 -0600

    oops some more changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,12 +12,10 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    binding.pry
     new_category = Category.create(name: params[:category][:name], user_id: session[:user_id])
     Cloudinary::Uploader.upload(params[:category][:img_src], :public_id => "#{session[:user_id]}-#{new_category.id}-Category")
     new_category.img_src = "#{session[:user_id]}-#{new_category.id}-Category"
     new_category.save
-    binding.pry
     redirect_to root_path
   end
 
--- db/migrate/20171109205303_create_categories.rb
+++ db/migrate/20171109205303_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
       t.string :name, null: false
-      t.string :img_src, default: "default"
+      t.string :img_src, default: "white.png"
       t.integer :user_id, default: nil
 
       t.timestamps
