e03f8d7c0306ee4c6cde2ca8274b06399d4c8e40
khristoph <christopherfriedman@gmail.com>
2017-11-14 15:27:09 -0600

    categories on cloudinary

--- db/migrate/20171109205303_create_categories.rb
+++ db/migrate/20171109205303_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
       t.string :name, null: false
-      t.string :img_src
+      t.string :img_src, default: "default"
       t.integer :user_id, default: nil
 
       t.timestamps
